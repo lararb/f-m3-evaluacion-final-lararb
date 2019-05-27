fc058311d84816a5d1ce4c9361c0d8ebf7877804
lararb <lararb91@gmail.com>
2019-05-27 12:44:08 +0200

    Add homepage to package.json

--- src/services/DataFetch.js
+++ src/services/DataFetch.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
+const ENDPOINT = 'https://hp-api.herokuapp.com/api/characters';
 
 const dataFetch = () => fetch(ENDPOINT).then(res => res.json())
 

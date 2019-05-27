1251c57e5c01d86aa0d0b4b5070b63667e2769b3
lararb <lararb91@gmail.com>
2019-05-27 12:40:05 +0200

    Create docs folder again

---
+++ docs/precache-manifest.25ed9c23d61a02cb7d0b95f48eeb049b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9854694c1d35796d8e3b08ee0ad34eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bc7d160220e94a044cd",
+    "url": "/static/css/main.10cc70d7.chunk.css"
+  },
+  {
+    "revision": "7a150f6c86a68a8bbee5",
+    "url": "/static/js/2.6d1c4afe.chunk.js"
+  },
+  {
+    "revision": "2bc7d160220e94a044cd",
+    "url": "/static/js/main.03623d1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e2d52d2c2d31464b9787d94d114fbdb",
+    "url": "/static/media/escudos.3e2d52d2.png"
+  },
+  {
+    "revision": "35c3d90f0dfc4b6be62fd4c2d17fcb5e",
+    "url": "/static/media/pergamino.35c3d90f.png"
+  }
+]);
